44e16fd3e3e52f8d97608167c8264c822efcbb06
Alvinleow <alvinleowyankai@gmail.com>
2024-02-02 04:17:00 +0800

    Create database

--- IPproject/src/com/config/DatabaseInitializer.java
+++ IPproject/src/com/config/DatabaseInitializer.java
@@ -14,20 +14,30 @@ public class DatabaseInitializer {
 
     @PostConstruct
     public void initialize() {
-        String sql = "CREATE TABLE IF NOT EXISTS `user` (" +
-                     "`id` int(11) NOT NULL AUTO_INCREMENT," +
-                     "`fullname` varchar(50) DEFAULT NULL," +
-                     "`address` varchar(100) DEFAULT NULL," +
-                     "`households` int(100) DEFAULT NULL," +
-                     "`buildingtype` varchar(20) DEFAULT NULL," +
-                     "`email` varchar(30) DEFAULT NULL," +
-                     "`phoneNumber` varchar(11) DEFAULT NULL," +
-                     "`username` varchar(30) DEFAULT NULL," +
-                     "`password` varchar(30) DEFAULT NULL," +
-                     "`userLevel` int(1) NOT NULL DEFAULT '0'," +
-                     "PRIMARY KEY (`id`)" +
-                     ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
-        
+        createUserTable();
+//        createOtherTable();
+    }
+    
+    private void createUserTable() {
+    	String sql = "CREATE TABLE IF NOT EXISTS `user` (" +
+                "`id` int(11) NOT NULL AUTO_INCREMENT," +
+                "`fullname` varchar(50) DEFAULT NULL," +
+                "`address` varchar(100) DEFAULT NULL," +
+                "`households` int(100) DEFAULT NULL," +
+                "`buildingtype` varchar(20) DEFAULT NULL," +
+                "`email` varchar(30) DEFAULT NULL," +
+                "`phoneNumber` varchar(11) DEFAULT NULL," +
+                "`username` varchar(30) DEFAULT NULL," +
+                "`password` varchar(30) DEFAULT NULL," +
+                "`userLevel` int(1) NOT NULL DEFAULT '0'," +
+                "PRIMARY KEY (`id`)" +
+                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+    	
         jdbcTemplate.execute(sql);
     }
+    
+//    private void createOtherTable() {
+//        String sql = "CREATE TABLE IF NOT EXISTS `other_table` (...);";
+//        jdbcTemplate.execute(sql);
+//    }
 }
